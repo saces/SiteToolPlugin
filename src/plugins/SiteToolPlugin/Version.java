db2d9a075849c8f53e38eadb64839a6822fc21c2
saces <saces@freenetproject.org>
2009-05-02 15:19:49 +0200

    initial commit

---
+++ src/plugins/SiteToolPlugin/Version.java
@@ -0,0 +1,21 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package plugins.SiteToolPlugin;
+
+/**
+ * @author saces
+ *
+ */
+public class Version {
+	/** SVN revision number. Only set if the plugin is compiled properly e.g. by emu. */
+	public static final String svnRevision = "@custom@";
+	
+	/** Version number of the plugin for getRealVersion(). Increment this on making
+	 * a major change, a significant bugfix etc. These numbers are used in auto-update 
+	 * etc, at a minimum any build inserted into auto-update should have a unique 
+	 * version.
+	 */
+	public static long version = 1;
+
+}
