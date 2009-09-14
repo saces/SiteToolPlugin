0c762f63e33714ba2ca4ca1ec2af02935a2216a7
saces <saces@freenetproject.org>
2009-09-14 23:42:07 +0200

    maybe better xml writer

--- src/plugins/SiteToolPlugin/fproxy/dav/util/XMLWriter.java
+++ src/plugins/SiteToolPlugin/fproxy/dav/util/XMLWriter.java
@@ -1,219 +1,457 @@
-/*
- * Copyright 1999,2004 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package plugins.SiteToolPlugin.fproxy.dav.util;
 
+import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.util.Map;
 
 /**
- * XMLWriter helper class.
- * 
- * @author <a href="mailto:remm@apache.org">Remy Maucherat</a>
+ * XMLWriter knows how to write elements and attributes, and how to use
+ * indenting.
  */
 public class XMLWriter {
 
-    // -------------------------------------------------------------- Constants
-
-	
 	/**
 	 * tag type: Opening, Closing, without content
 	 *
 	 */
-	public enum TAG { OPENING, CLOSING, NO_CONTENT }
+	public enum TAG { OPENING, CLOSING, NO_CONTENT };
 
-    // ----------------------------------------------------- Instance Variables
+	/** use a two space indent */
+	public static final String INDENT = "  ";
 
-    /**
-     * Buffer.
-     */
-    private Writer _writer;
+	// write states
 
-    /**
-     * Namespaces to be declared in the root element
-     */
-    private Map<String, String> _namespaces;
+	/** writing content */
+	protected final static int CONTENT = 0;
 
-    /**
-     * Is true until the root element is written
-     */
-    private boolean _isRootElement = true;
-    private int _indent = 0;
+	/** wrote start */
+	protected final static int START = 1;
 
-    // ----------------------------------------------------------- Constructors
+	/** writing attributes */
+	protected final static int ATTRIBUTE = 2;
 
-    /**
-     * Constructor.
-     */
-    public XMLWriter(OutputStream os, Map<String, String> namespaces) {
-        _namespaces = namespaces;
-        try {
-			_writer = new OutputStreamWriter(os, "UTF-8");
+	/** wrote end */
+	protected final static int END = 3;
+
+	/** Write to XML document */
+	protected PrintWriter mOut;
+
+	/** indent */
+	protected Indent indent;
+
+	/** writes PCDATA replacing XML characters with escape entities */
+	protected PCDATAFilterWriter mFilter;
+
+	/** current state */
+	protected int state = CONTENT;
+
+	/** Namespaces to be declared in the root element */
+	private Map<String, String> _namespaces;
+
+	/** Is true until the root element is written */
+	private boolean _isRootElement = true;
+
+	/**
+	 * @param namespaces
+	 * 
+	 * 
+	 */
+	public XMLWriter(OutputStream out, Map<String, String> namespaces) {
+		try {
+			mOut = new PrintWriter(new OutputStreamWriter(out, "UTF-8"));
 		} catch (UnsupportedEncodingException e) {
 			throw new RuntimeException("Impossible, JVM doesnt support UTF-8");
 		}
-    }
+		mFilter = new PCDATAFilterWriter(mOut);
+		indent = new Indent();
+		_namespaces = namespaces;
+	}
 
-    // --------------------------------------------------------- Public Methods
+	/**
+	 * Write &lt;?xml version="1.0"?&gt;
+	 */
+	public void writeXMLHeader() {
+		mOut.println("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+	}
 
-    /**
-     * Write property to the XML.
-     * 
-     * @param name
-     *      Property name
-     * @param value
-     *      Property value
-     * @throws IOException 
+	/**
+	 * Write a DOCTYPE
+	 * 
+	 * @param name
+	 *            document type name
+	 * @param publicId
+	 *            public Id. May be Null.
+	 * @param systemId
+	 *            system Id.
+	 */
+	public void writeDocumentType(String name, String publicId, String systemId) {
+		mOut.print("<!DOCTYPE ");
+		mOut.print(name);
+		mOut.print(' ');
+
+		if (publicId != null) {
+			mOut.print('"');
+			mOut.print(publicId);
+			mOut.print("\" ");
+		}
+
+		mOut.print("SYSTEM \"");
+		mOut.print(systemId);
+		mOut.println("\">");
+	}
+
+	/**
+     *
      */
-    public void writeProperty(String name, String value) throws IOException {
-        writeElement(name, TAG.OPENING);
-        _writer.append(value);
-        writeElement(name, TAG.CLOSING);
-        newLine();
-    }
+	public void attribute(String name, int value) throws IOException {
+		attribute(name, String.valueOf(value));
+	}
 
-    /**
-     * Write property to the XML.
-     * 
-     * @param name
-     *      Property name
-     * @throws IOException 
+	/**
+     *
      */
+	public void attribute(String name, boolean value) throws IOException {
+		attribute(name, String.valueOf(value));
+	}
+
+	/**
+     *
+     */
+	public void attribute(String name, Object value) throws IOException {
+		if (value == null) {
+			return;
+		}
+
+		attribute(name, value.toString());
+	}
+
+	/**
+     *
+     */
+	public void attribute(String name, String value) throws IOException {
+		if (value == null) {
+			return;
+		}
+
+		state = ATTRIBUTE;
+		mOut.print(' ');
+		mOut.print(name);
+		mOut.print('=');
+		mOut.print('"');
+		mFilter.write(value);
+		mOut.print('"');
+	}
+
+	/**
+     *
+     */
+	public void content(String content) throws IOException {
+		// close the last tag
+		if ((state == START) || (state == ATTRIBUTE)) {
+			mOut.print('>');
+		}
+
+		mFilter.write(content);
+		state = CONTENT;
+	}
+
+	/**
+	 * Write start tag.
+	 */
+	public void start(String name) {
+		StringBuffer nsdecl = new StringBuffer();
+
+		if (_isRootElement) {
+			for (String fullName : _namespaces.keySet()) {
+				String abbrev = _namespaces.get(fullName);
+				nsdecl.append(" xmlns:");
+				nsdecl.append(abbrev);
+				nsdecl.append("=\"");
+				nsdecl.append(fullName);
+				nsdecl.append("\"");
+			}
+			_isRootElement = false;
+		}
+
+		int pos = name.lastIndexOf(':');
+		if (pos >= 0) {
+			// lookup prefix for namespace
+			String fullns = name.substring(0, pos);
+			String prefix = _namespaces.get(fullns);
+			if (prefix == null) {
+				// there is no prefix for this namespace
+				name = name.substring(pos + 1);
+				nsdecl.append(" xmlns=\"").append(fullns).append("\"");
+			} else {
+				// there is a prefix
+				name = prefix + ":" + name.substring(pos + 1);
+			}
+		} else {
+			throw new IllegalArgumentException("All XML elements must have a namespace");
+		}
+
+		// close the last tag
+		if ((state == START) || (state == ATTRIBUTE)) {
+			mOut.println('>');
+		}
+
+		mOut.print(indent);
+		mOut.print('<');
+		mOut.print(name + nsdecl);
+		indent.indent();
+		state = START;
+	}
+
+	/**
+	 * Write end tag.
+	 * 
+	 */
+	public void end(String name) {
+		indent.unIndent();
+
+		StringBuffer nsdecl = new StringBuffer();
+
+		int pos = name.lastIndexOf(':');
+		if (pos >= 0) {
+			// lookup prefix for namespace
+			String fullns = name.substring(0, pos);
+			String prefix = _namespaces.get(fullns);
+			if (prefix == null) {
+				// there is no prefix for this namespace
+				name = name.substring(pos + 1);
+				nsdecl.append(" xmlns=\"").append(fullns).append("\"");
+			} else {
+				// there is a prefix
+				name = prefix + ":" + name.substring(pos + 1);
+			}
+		} else {
+			throw new IllegalArgumentException("All XML elements must have a namespace");
+		}
+
+
+		if ((state == START) || (state == ATTRIBUTE)) {
+			mOut.println("/>");
+		} else if (state == CONTENT) {
+			mOut.print("</");
+			mOut.print(name + nsdecl);
+			mOut.println('>');
+		} else {
+			mOut.print(indent);
+			mOut.print("</");
+			mOut.print(name + nsdecl);
+			mOut.println('>');
+		}
+
+		state = END;
+	}
+
+	/**
+	 * Implements indenting for elements.
+	 * 
+	 * @author Brian King
+	 */
+	protected class Indent {
+		/** Base indent */
+		protected String mIndent = new String();
+
+		/**
+		 * Add a level of indentation
+		 */
+		public void indent() {
+			mIndent += INDENT;
+		}
+
+		/**
+		 * Remove a level of indentation
+		 * 
+		 */
+		public void unIndent() {
+			int len = mIndent.length();
+			int iLen = INDENT.length();
+
+			if (len >= iLen) {
+				mIndent = mIndent.substring(0, len - iLen);
+			}
+		}
+
+		/**
+		 * Return the current indent
+		 * 
+		 */
+		@Override
+		public String toString() {
+			return mIndent;
+		}
+	}
+
+	public void writeElement(String name, TAG type) throws IOException {
+		switch (type) {
+		case OPENING:
+			start(name);
+			break;
+		case CLOSING:
+			end(name);
+			break;
+		case NO_CONTENT:
+			start(name);
+			end(name);
+		}
+	}
+
+	public void writeProperty(String name, String value) throws IOException {
+		start(name);
+        content(value);
+        end(name);
+    }
+	
+	public void writeData(String data) throws IOException {
+		// close the last tag
+		if ((state == START) || (state == ATTRIBUTE)) {
+			mOut.print('>');
+		}
+
+		mOut.write("<![CDATA[" + data + "]]>");
+		state = CONTENT;
+	}
+	
     public void writeProperty(String name) throws IOException {
         writeElement(name, TAG.NO_CONTENT);
-        newLine();
     }
+    
+    public void close() throws IOException {
+		mOut.close();
+	}
 
-    /**
-     * Write an element.
-     * 
-     * @param name
-     *      Element name
-     * @param type
-     *      Element type
-     * @throws IOException 
-     */
-    public void writeElement(String name, TAG type) throws IOException {
-        StringBuffer nsdecl = new StringBuffer();
 
-        if (_isRootElement) {
-            for (String fullName: _namespaces.keySet()) {
-                String abbrev = (String) _namespaces.get(fullName);
-                nsdecl.append(" xmlns:");
-                nsdecl.append(abbrev);
-                nsdecl.append("=\"");
-                nsdecl.append(fullName);
-                nsdecl.append("\"");
-            }
-            _isRootElement = false;
-        }
+	public void writeText(String text) throws IOException {
+		content(text);
+	}
 
-        int pos = name.lastIndexOf(':');
-        if (pos >= 0) {
-            // lookup prefix for namespace
-            String fullns = name.substring(0, pos);
-            String prefix = (String) _namespaces.get(fullns);
-            if (prefix == null) {
-                // there is no prefix for this namespace
-                name = name.substring(pos + 1);
-                nsdecl.append(" xmlns=\"").append(fullns).append("\"");
-            } else {
-                // there is a prefix
-                name = prefix + ":" + name.substring(pos + 1);
-            }
-        } else {
-            throw new IllegalArgumentException("All XML elements must have a namespace");
-        }
+	/**
+	 * Write XML PCDATA using entity substitutions.
+	 * 
+	 */
+	protected class PCDATAFilterWriter extends FilterWriter {
+		/** entity substitution for < */
+		protected char[] LT = { '&', 'l', 't', ';' };
 
-        switch (type) {
-        case OPENING:
-            _writer.append("<" + name + nsdecl + ">");
-            break;
-        case CLOSING:
-            _writer.append("</" + name + ">\n");
-            break;
-        case NO_CONTENT:
-        default:
-            _writer.append("<" + name + nsdecl + "/>");
-            break;
-        }
-    }
+		/** entity substitution for > */
+		protected char[] GT = { '&', 'g', 't', ';' };
 
-    public void newLine() throws IOException {
-        _writer.append('\n');
-        for(int i=0;i<_indent;i++) _writer.append(' ');
-    }
+		/** entity substitution for > */
+		protected char[] AMP = { '&', 'a', 'm', 'p', ';' };
 
-    public void indentNl() throws IOException {
-    	_writer.append('\n');
-        _indent++;
-        for(int i=0;i<_indent;i++) _writer.append(' ');
-    }
+		/**
+		 * Constructor.
+		 */
+		public PCDATAFilterWriter(Writer out) {
+			super(out);
+		}
 
-    public void unIndentNl() throws IOException {
-    	_writer.append('\n');
-        _indent--;
-        for(int i=0;i<_indent;i++) _writer.append(' ');
-    }
+		/**
+		 * Write a single character.
+		 * 
+		 * @exception IOException
+		 *                If an I/O error occurs
+		 */
+		@Override
+		public void write(int c) throws IOException {
+			synchronized (lock) {
+				if (c == '<') {
+					out.write(LT);
+				} else if (c == '>') {
+					out.write(GT);
+				} else if (c == '&') {
+					out.write(AMP);
+				} else {
+					out.write(c);
+				}
+			}
+		}
 
-    public void writeElementIndent(String name, TAG type) throws IOException {
-    	writeElement(name, type);
-    	indentNl();
-    }
+		/**
+		 * Write a portion of an array of characters.
+		 * 
+		 * @param cbuf
+		 *            Buffer of characters to be written
+		 * @param off
+		 *            Offset from which to start reading characters
+		 * @param len
+		 *            Number of characters to be written
+		 * 
+		 * @exception IOException
+		 *                If an I/O error occurs
+		 */
+		@Override
+		public void write(char[] cbuf, int off, int len) throws IOException {
+			synchronized (lock) {
+				for (int i = 0; i < len; i++) {
+					write(cbuf[off + i]);
+				}
+			}
+		}
 
-    public void writeElementNl(String name, TAG type) throws IOException {
-    	writeElement(name, type);
-    	newLine();
-    }
+		/**
+		 * Write a portion of a string.
+		 * 
+		 * @param str
+		 *            String to be written
+		 * @param off
+		 *            Offset from which to start reading characters
+		 * @param len
+		 *            Number of characters to be written
+		 * 
+		 * @exception IOException
+		 *                If an I/O error occurs
+		 */
+		@Override
+		public void write(String str, int off, int len) throws IOException {
+			if (str == null) {
+				return;
+			}
+			synchronized (lock) {
+				for (int i = 0; i < len; i++) {
+					write(str.charAt(off + i));
+				}
+			}
+		}
 
-    /**
-     * Write text.
-     * 
-     * @param text
-     *      Text to append
-     * @throws IOException 
-     */
-    public void writeText(String text) throws IOException {
-        _writer.append(text);
-    }
+		/**
+		 * Write a string.
+		 * 
+		 * @param str
+		 *            String to be written
+		 * 
+		 * @exception IOException
+		 *                If an I/O error occurs
+		 */
+		@Override
+		public void write(String str) throws IOException {
+			if (str == null) {
+				return;
+			}
+			synchronized (lock) {
+				int len = str.length();
 
-    /**
-     * Write data.
-     * 
-     * @param data
-     *      Data to append
-     * @throws IOException 
-     */
-    public void writeData(String data) throws IOException {
-        _writer.append("<![CDATA[" + data + "]]>");
-    }
+				for (int i = 0; i < len; i++) {
+					int c = str.charAt(i);
 
-    /**
-     * Write XML Header.
-     * @throws IOException 
-     */
-    public void writeXMLHeader() throws IOException {
-        _writer.append("<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n");
-    }
-
-	public void close() throws IOException {
-		_writer.close();
+					if (c == '<') {
+						out.write(LT);
+					} else if (c == '>') {
+						out.write(GT);
+					} else if (c == '&') {
+						out.write(AMP);
+					} else {
+						out.write(c);
+					}
+				}
+			}
+		}
 	}
 }
