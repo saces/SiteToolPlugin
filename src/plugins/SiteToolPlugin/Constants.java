c5c8d3ad9732941a2caf3be88ee44102742fdbae
saces <saces@freenetproject.org>
2009-09-29 13:31:55 +0200

    add the obligatory forgotten one.

---
+++ src/plugins/SiteToolPlugin/Constants.java
@@ -0,0 +1,10 @@
+package plugins.SiteToolPlugin;
+
+public class Constants {
+
+	public final static String DL_TYPE_TAR7Z = "tar7z";
+	public final static String DL_TYPE_TARGZ = "targz";
+	public final static String DL_TYPE_TAR = "tar";
+	public final static String DL_TYPE_ZIP = "zip";
+
+}
